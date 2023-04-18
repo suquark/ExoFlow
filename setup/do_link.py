76bbba57b2a1210985ed834c2a26d196fe1d253d
Siyuan <suquark@gmail.com>
2023-04-18 18:06:44 -0400

    update

--- setup/do_link.py
+++ setup/do_link.py
@@ -1,8 +1,10 @@
 import importlib
 import os
 
-os.chdir(os.path.expanduser("~/efs/ray/python/ray"))
-setup_dev = importlib.import_module("setup-dev")
+setup_dev = importlib.import_module("ray.setup-dev")
+
+# chdir because 'do_link' is relative to the current directory
+os.chdir(os.path.expanduser("~/ExoFlow/patch"))
+
 setup_dev.do_link("_private/storage.py", force=True)
 setup_dev.do_link("dag", force=True)
-setup_dev.do_link("workflow", force=True)
