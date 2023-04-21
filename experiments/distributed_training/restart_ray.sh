5b2a57da89ee9c75254f37edc09f179aece3f6e5
Siyuan <suquark@gmail.com>
2023-04-21 00:00:45 -0700

    update

--- experiments/distributed_training/restart_ray.sh
+++ experiments/distributed_training/restart_ray.sh
@@ -10,8 +10,8 @@ ray stop --force
 ssh $RAY_WORKER_IP "source ~/anaconda3/bin/activate exoflow-dev; ray stop --force"
 sleep 10
 
-PYTHONPATH=~/efs/exp1 ray start --head --disable-usage-stats --port=6379 --object-manager-port=8076 --autoscaling-config=~/ray_bootstrap_config.yaml --resources='{"machine": 1, "tag:gpu": 1}' --storage=s3://siyuan-workflow/exp1 --num-cpus 32
+PYTHONPATH=/exoflow/experiments/distributed_training ray start --head --disable-usage-stats --port=6379 --object-manager-port=8076 --autoscaling-config=~/ray_bootstrap_config.yaml --resources='{"machine": 1, "tag:gpu": 1}' --storage=s3://exoflow --num-cpus 32
 
-ssh $RAY_WORKER_IP "source ~/anaconda3/bin/activate exoflow-dev; PYTHONPATH=~/efs/exp1 ray start --address=$RAY_HEAD_IP:6379 --disable-usage-stats --object-manager-port=8076 --resources='{\"machine\": 1, \"tag:data\": 1}' --storage=s3://siyuan-workflow/exp1 --num-cpus 32"
+ssh $RAY_WORKER_IP "source ~/anaconda3/bin/activate exoflow-dev; PYTHONPATH=/exoflow/experiments/distributed_training ray start --address=$RAY_HEAD_IP:6379 --disable-usage-stats --object-manager-port=8076 --resources='{\"machine\": 1, \"tag:data\": 1}' --storage=s3://exoflow --num-cpus 32"
 
 sleep 10
