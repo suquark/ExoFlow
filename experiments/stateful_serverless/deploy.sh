d3655d788d28a9832993f1784a9671baaac98697
Siyuan <suquark@gmail.com>
2023-04-23 16:19:58 -0700

    update

--- experiments/stateful_serverless/deploy.sh
+++ experiments/stateful_serverless/deploy.sh
@@ -13,28 +13,28 @@ docker rm beldi &> /dev/null
 docker run -td -v /exoflow/experiments/stateful_serverless:/stateful_serverless --name=beldi tauta/beldi:latest
 container_id=$(docker ps -aqf "name=beldi")
 # docker exec beldi <command>
-echo
+echo 'Done\n'
 
 echo '========= Setting up Credentials ========='
 cmd "mkdir -p ~/.aws"
 docker cp ~/.aws/credentials $container_id:/root/.aws/credentials
 cmd "chmod 600 ~/.aws/credentials"
-echo
+echo 'Done\n'
 
 echo '========= Compiling ========='
 cmd "make clean && make hotel"
 cmd make -f /stateful_serverless/Makefile hotel
-echo
+echo 'Done\n'
 
 echo "========= Deploying ========="
 cmd sls deploy -c hotel.yml
 cmd make -f /stateful_serverless/Makefile deploy
-echo
+echo 'Done\n'
 
 echo "========= Initializing Database (DynamoDB) ========="
 cmd make -f /stateful_serverless/Makefile init_data
 python /exoflow/experiments/stateful_serverless/init_dynamodb_table.py
-echo
+echo 'Done\n'
 
 # increase the concurrent execution quota (by default 10, Beldi requires 1000)
 # https://us-east-1.console.aws.amazon.com/servicequotas/home/services/lambda/quotas
