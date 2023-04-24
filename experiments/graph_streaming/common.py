675da825afe7cf0ddd3ba6e0ab2225f03d554c0a
Siyuan <suquark@gmail.com>
2023-04-24 04:28:25 -0700

    update

--- experiments/graph_streaming/common.py
+++ experiments/graph_streaming/common.py
@@ -31,7 +31,7 @@ else:
     N_EPOCH_INTERVAL = 10
     N_CHECKPOINT_INTERVAL = 10
     SPARK_MEMORY = "100g"
-    STORAGE = "s3://siyuan-workflow"
+    STORAGE = "s3://exoflow"
 
 # constant in TunkRank
 # https://www.rivaliq.com/blog/good-engagement-rate-twitter/
