4d9ff16c2b9113a291286911b4250db971942940
Siyuan <suquark@gmail.com>
2023-05-28 23:02:59 +0800

    update

--- exoflow/task_executor.py
+++ exoflow/task_executor.py
@@ -264,7 +264,7 @@ class ActorController:
         #     StateResource,
         #     ListApiOptions,
         # )
-        from ray.experimental.state.api import list_nodes
+        # from ray.experimental.state.api import list_nodes
         from ray._private.worker import global_worker
 
         self._pools: Dict[str, ActorPool] = {}
@@ -277,8 +277,12 @@ class ActorController:
         # nodes = client.list(
         #     StateResource("nodes"), ListApiOptions(), raise_on_missing_output=False
         # )
-        head_node = global_worker.node.gcs_address.split(":")[0]
-        nodes = list_nodes(f"http://{head_node}:8265")
+        # head_node = global_worker.node.gcs_address.split(":")[0]
+        # nodes = list_nodes(f"http://{head_node}:8265")
+        nodes = {}
+        for n in ray.nodes():
+            nodes["node_id"] = n["NodeID"]
+            nodes["state"] = "ALIVE" if n["Alive"] else "FAILED"
         if local_workers_only:
             local_node_id = global_worker.core_worker.get_current_node_id().hex()
             nodes = [node for node in nodes if node["node_id"] == local_node_id]
