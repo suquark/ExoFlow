8eaa4e440d8663924e6a8f852a0abe5ba3d10db8
Siyuan <suquark@gmail.com>
2023-05-29 00:30:58 +0800

    update

--- experiments/microbenchmarks/scalability/benchmark.sh
+++ experiments/microbenchmarks/scalability/benchmark.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 pip install shortuuid
+# num_nodes=$(ray get-worker-ips ~/ray_bootstrap_config.yaml | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | wc -l)
 
-num_nodes=$(ray get-worker-ips ~/ray_bootstrap_config.yaml | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | wc -l)
-./restart_ray.sh
-python run.py --n-schedulers=$num_nodes --n-workers=2 --n-nodes=$num_nodes
+for num_nodes in 1 2 4 8 16; do
+    ./restart_ray.sh
+    python run.py --n-schedulers=$num_nodes --n-workers=2 --n-nodes=$num_nodes
+
+    # ./restart_ray.sh
+    # python run_ray.py --n-schedulers=$i --n-workers=2
+done
---
+++ experiments/microbenchmarks/scalability/run_ray.py
@@ -0,0 +1,33 @@
+import argparse
+import json
+import time
+
+import ray
+import tqdm
+
+from config import N_TASKS
+
+
+@ray.remote(resources={"tag:worker": 0.5})
+def nop():
+    pass
+
+
+def run_ray_tasks(n_tasks: int, n_repeats: int = 5, n_warmups: int = 1):
+    durations = []
+    for _ in tqdm.trange(n_warmups + n_repeats, desc="Ray Task throughput"):
+        start = time.time()
+        ray.get([nop.remote() for _ in range(n_tasks)])
+        durations.append(time.time() - start)
+    return durations[n_warmups:]
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="benchmark throughput")
+    parser.add_argument("--n-cpus", help="number of CPUs", type=int, default=1)
+    args = parser.parse_args()
+    ray.init(num_cpus=args.n_cpus)
+
+    durations = run_ray_tasks(N_TASKS)
+    with open(f"result/ray_{args.n_cpus}.json", "w") as f:
+        json.dump(durations, f)
