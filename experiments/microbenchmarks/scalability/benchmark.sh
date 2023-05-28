25210ca86944b148d93ba2173aa92c14e689341d
Siyuan <suquark@gmail.com>
2023-05-29 00:13:35 +0800

    fix

--- experiments/microbenchmarks/scalability/benchmark.sh
+++ experiments/microbenchmarks/scalability/benchmark.sh
@@ -3,4 +3,4 @@ pip install shortuuid
 
 num_nodes=$(ray get-worker-ips ~/ray_bootstrap_config.yaml | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | wc -l)
 ./restart_ray.sh
-python run.py --n-schedulers=1 --n-workers=1 --n-nodes=$num_nodes
+python run.py --n-schedulers=$num_nodes --n-workers=2 --n-nodes=$num_nodes
