cdc693566a1d4e5127af2a35099024a3818e4642
Siyuan <suquark@gmail.com>
2023-05-29 23:47:16 +0800

    update

--- experiments/microbenchmarks/scalability/benchmark.sh
+++ experiments/microbenchmarks/scalability/benchmark.sh
@@ -2,12 +2,12 @@
 pip install shortuuid
 # num_nodes=$(ray get-worker-ips ~/ray_bootstrap_config.yaml | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | wc -l)
 
-for num_nodes in 1 2 4 8 16; do
+for num_nodes in 1 2 4 8 16 32; do
     ./restart_ray.sh
     python run_ray.py --n-controllers=$num_nodes --n-executors=2
 done
 
-for num_nodes in 1 2 4 8 16; do
+for num_nodes in 1 2 4 8 16 32; do
     ./restart_ray.sh
     python run.py --n-controllers=$num_nodes --n-executors=2
 done
